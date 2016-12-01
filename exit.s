f5432dc2bf667e7408d21ff4fb6a1bd76b7274c4
ajrpayne <ajrpayne@gmail.com>
2016-12-01 18:40:46 +0000

    added example exit.s

---
+++ exit.s
@@ -0,0 +1,44 @@
+# PURPOSE:  Simple program that exits and returns a
+#           status code back to the Linux kernel
+#
+
+# INPUT:    none
+#
+
+# OUTPUT:   returns a status code. This can be viewed
+#           by typing
+#
+#           echo $?
+#
+#           after running the program
+#
+
+# VARIABLES:
+#           %rax holds the system call number
+#           %rdi holds the return status
+#
+
+# Changes from 32 bit:
+#           movl -> movq
+#           command number for exiting $1 -> $60
+#           eax  -> rax
+#           ebx  -> rdi
+#           int $0x80 -> syscall
+
+.section .data
+
+.section .text
+.globl _start
+_start:
+movq $60, %rax      # this is the linux kernel command
+                    # number (system call) for exiting
+                    # a program
+
+movq $0, %rdi       # this is the status number we will
+                    # return to the operating system.
+                    # Change this around and it will
+                    # return different things to
+                    # echo $?
+
+syscall             # this wakes up the kernel to run
+                    # the exit command
